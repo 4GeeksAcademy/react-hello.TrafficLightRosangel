a6394ebb2baad9951fcb74329f46e924254001c3
Rosangel593 <148271323+Rosangel593@users.noreply.github.com>
2023-12-18 20:29:01 +0000

    Terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,12 +10,14 @@ const Home = () => {
 	
 	return ( 
 <>
-
+<div className= "stick"></div>
+<div className="Contenedor"> 																						
 		<div className="traffic-light">
 		<div onClick = {() => setClickColor("red")} className={"red "+((clickColor === "red") ? "glow" : "")}></div>
 		<div onClick = {() => setClickColor("yellow")} className={"yellow "+((clickColor === "yellow") ? "glow" : "")}></div>
 		<div onClick = {() => setClickColor("green")} className={"green "+((clickColor === "green") ? "glow" : "")}></div >
 		
+		</div >
 		</div >
 		</>
 	);
